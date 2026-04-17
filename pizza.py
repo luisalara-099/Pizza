9813cf678264a82489b8e12b4007ff75f7c53cb4
luisalara-099 <luisaanalialaraa@gmail.com>
2026-04-17 18:56:56 -0400

    todo listo

--- pizza.py
+++ pizza.py
@@ -1,58 +1,28 @@
 import flet as ft
-
+ 
 def main(page: ft.Page):
-
+ 
     def cambiar(e):
         pepperoni.visible = switchPep.value
-        mushrooms.visible = switchMush.value
-        olives.visible = switchOlives.value
+        Hongos.visible = switchHongos.value
+        olives.visible = switcholives.value
         page.update()
-
-    base = ft.Image(
-        src="assets/cheesebase.jpg",
-        width=300,
-        height=300
-    )
-
-    pepperoni = ft.Image(
-        src="assets/pepperoni.png",
-        width=300,
-        height=300,
-        visible=False
-    )
-
-    mushrooms = ft.Image(
-        src="assets/mushrooms.png",
-        width=300,
-        height=300,
-        visible=False
-    )
-
-    olives = ft.Image(
-        src="assets/olives.png",
-        width=300,
-        height=300,
-        visible=False
-    )
-
+ 
+    base = ft.Image(src="images/cheesebase.jpg", width=200, height=200)
+    pepperoni = ft.Image(src="images/pepperoni.png", visible=False, width=150, height=200)
+    Hongos = ft.Image(src="images/mushrooms.png", visible=False, width=150, height=200)
+    olives = ft.Image(src="images/olives.png", visible=False, width=100, height=150)
+ 
     switchPep = ft.Switch(label="Pepperoni", on_change=cambiar)
-    switchMush = ft.Switch(label="Mushrooms", on_change=cambiar)
-    switchOlives = ft.Switch(label="Olives", on_change=cambiar)
-
+    switchHongos = ft.Switch(label="Mushrooms", on_change=cambiar)
+    switcholives = ft.Switch(label="olives", on_change=cambiar)
+ 
     page.add(
         ft.Row([
-            ft.Stack([
-                base,
-                pepperoni,
-                mushrooms,
-                olives
-            ]),
-            ft.Column([
-                switchPep,
-                switchMush,
-                switchOlives
-            ])
+            ft.Stack([base, pepperoni, Hongos, olives]),
+            ft.Column([switchPep, switchHongos, switcholives])
         ])
     )
-
-ft.app(target=main)
+ 
+ft.run(main=main, assets_dir="assets")
+ 
